a6bd31c844c096897a65a2a00ebc60488064d3e4
Leemuns <firedew123@gmail.com>
2026-05-20 16:07:19 +0800

    Add buttons and their displays (exercise 1.6)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,44 @@
 import { useState } from 'react'
 
+const Button = ({onClick, text}) => {
+  return (
+    <>
+      <button onClick={onClick}>{text}</button>
+    </>
+  )
+}
+
+const StatsLine = ({text, val}) => {
+  return (
+    <>
+      <p>{text} {val}</p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const incrementCategory = (category, setterFn) => {
+    setterFn(category + 1)
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+
+      <Button onClick={() => incrementCategory(good, setGood)} text="good"/>
+      <Button onClick={() => incrementCategory(neutral, setNeutral)} text="neutral"/>
+      <Button onClick={() => incrementCategory(bad, setBad)} text="bad"/>
+
+      <h1>statistics</h1>
+
+      <StatsLine text="good" val={good} />
+      <StatsLine text="neutral" val={neutral} />
+      <StatsLine text="bad" val={bad} />
     </div>
   )
 }
