727f3b263d26f6fa8dd288771069cdd40d0292cc
Leemuns <firedew123@gmail.com>
2026-05-20 22:29:04 +0800

    Rename to courseCardList and separate it own module (exercise 2.5)

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,22 +1,4 @@
-const CourseList = ({courses}) => <div>{courses.map(course => <Course key={course.id} course={course}/>)}</div>
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
-
-const Header = ({name}) => <h2>{name}</h2>
-
-const Content = ({parts}) => <div>{parts.map(part => <Part key={part.id} part={part}/>)}</div>
-
-const Part = ({part}) => <p>{part.name} {part.exercises}</p>
-
-const Total = ({parts}) => <p><b>Number of {parts.reduce((acc, part) => acc + part.exercises, 0)} exercises</b></p>
+import CourseCardList from './components/CourseCardList.jsx'
 
 const App = () => {
   const courses = [
@@ -67,7 +49,7 @@ const App = () => {
   return (
     <div>
       <h1>Web development curriculum</h1>
-      <CourseList courses={courses} />
+      <CourseCardList courses={courses} />
     </div>
   )
 }
