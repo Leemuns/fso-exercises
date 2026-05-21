87e0e32b7a849f4afb863b3ded13bf516b661a28
Leemuns <firedew123@gmail.com>
2026-05-21 13:09:17 +0800

    Add project thepohonebook (exercise 2.6)

---
+++ part2/thephonebook/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { id: '1', name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({
+      id: String(persons.length + 1),
+      name: newName,
+    }));
+    console.log(persons)
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      
+      <div>
+        {persons.map(person => <p key={person.id}>{person.name}</p>)}
+      </div>
+    </div>
+  )
+}
+
+export default App
