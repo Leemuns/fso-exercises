275cc3527ef078fd7984d5fb75d7b82f7d83169b
Leemuns <firedew123@gmail.com>
2026-05-22 13:17:59 +0800

    Refactor backend code into own module (exercise 2.13)

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import personsService from './services/persons.jsx'
 import Filter from './components/Filter.jsx'
 import PersonForm from './components/PersonForm.jsx'
 import PersonCardList from './components/PersonCardList.jsx'
+import person from './services/persons.jsx'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,30 +13,31 @@ const App = () => {
   const [nameFilter, setNameFilter] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then( response => {
-      setPersons(response.data)
-    })
+    personsService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
   }, [])
 
   const addPerson = (event) => {
     event.preventDefault();
-    if (persons.reduce((isAdded, person) => isAdded || person.name === newPerson.name, false)) {
-      alert(`${newPerson.name} is already added to the phonebook`)
+    if (!newPerson.name || !newPerson.number) {
+      alert(`New Person details cannot be empty.`)
       return
     }
 
-    axios
-      .post('http://localhost:3001/persons', newPerson)
-      .then( response => {
-        setPersons(persons.concat(response.data))
-        setNewPerson('')
-      })
+    if (persons.reduce((isAdded, person) => isAdded || person.name === newPerson.name, false)) {
+      alert(`${newPerson.name} is already added to the phonebook.`)
+      return
+    }
 
-    // setPersons(persons.concat({
-    //   name: newPerson.name,
-    //   number: newPerson.number,
-    //   id: String(persons.length + 1)
-    // }));
+    personsService
+      .create(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewPerson({ name: '', number: '' })
+      })
   }
 
   return (
