3ddaf4f884e925ab00d0bf5c449002e18b8c8af3
Leemuns <firedew123@gmail.com>
2026-05-23 21:54:32 +0800

    Add GET '/api/persons/:id' route (exercise 3.3)

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -41,6 +41,16 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const person = persons.find(person => person.id === req.params.id)
+  
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
