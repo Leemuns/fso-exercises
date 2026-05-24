21809c1628d80bf3102fde9a2a90458a61445732
Leemuns <firedew123@gmail.com>
2026-05-25 00:02:14 +0800

    Add production build of frontend to backend (exercise 3.11)

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -31,6 +31,7 @@ morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 app.use(cors())
 
 app.get('/', (req, res) => {
--- part3/phonebookfrontend/src/services/persons.jsx
+++ part3/phonebookfrontend/src/services/persons.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://phonebook-8f3t.onrender.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   return axios
