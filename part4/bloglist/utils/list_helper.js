0604a77feb848a86b29a4a817cd30b56c6e42cef
Leemuns <firedew123@gmail.com>
2026-05-26 18:03:06 +0800

    Add totalLikes function and tests for it (exercise 4.4)

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,18 @@
-const dummy = blogs => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = blogList => {
+  if (!blogList.length) {
+    return 0
+  }
+
+  return blogList.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
