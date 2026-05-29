cb0ed58b7f400e61ffc5e330e0e18254ed57e6b9
Leemuns <firedew123@gmail.com>
2026-05-29 19:11:38 +0800

    Refactor finding the user identity using token to middleware (exercise 4.22)

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,23 +1,17 @@
 const blogsRouter = require('express').Router()
-const jwt = require('jsonwebtoken')
 
 const Blog = require('../models/blog')
-const User = require('../models/user')
 
-blogsRouter.get('', async (request, response) => {
+blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
     .populate('user', { username: 1, name: 1, id: 1 })
 
   response.json(blogs)
 })
 
-blogsRouter.post('', async (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blog = new Blog(body)
   blog.user = user._id
@@ -34,13 +28,8 @@ blogsRouter.post('', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const userId = decodedToken.id
-
   const blog = await Blog.findById(request.params.id)
+  const userId = request.user.id
 
   if (blog.user.toString() !== userId.toString()) {
     console.log('TEST')
