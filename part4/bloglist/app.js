e3a111fbb7dab90d1e48c86e6977bc67ff583748
Leemuns <firedew123@gmail.com>
2026-05-29 18:34:29 +0800

    Refactor extract token into middleware (exercise 4.20)

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -20,6 +20,7 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
