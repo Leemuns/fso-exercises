cf3cfa3335d05d7a19589246881c03cff9a1a8f2
Leemuns <firedew123@gmail.com>
2026-05-29 18:10:15 +0800

    Modify get endpoint to populate their ref fields (exercise 4.17)

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,16 +1,32 @@
 const blogsRouter = require('express').Router()
+
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('', async (request, response) => {
   const blogs = await Blog.find({})
+    .populate('user', { username: 1, name: 1, id: 1 })
+
   response.json(blogs)
 })
 
 blogsRouter.post('', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const user = await User.findOne()
+  // const user = await User.findById(body.userId)
 
-  const result = await blog.save()
-  response.status(201).json(result)
+  const blog = new Blog(body)
+  blog.user = user._id
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
