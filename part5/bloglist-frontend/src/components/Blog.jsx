759414bc0900e9518b3c43af048716e2b25b1c52
Leemuns <firedew123@gmail.com>
2026-06-02 11:13:57 +0800

    Add feature to show or hide blog details (exercise 5.7)

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+import Togglable from './Togglable'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const togglevisibility =() => {
+    setVisible(!visible)
+  }
+
+  const style = {
+    border: '1px solid black',
+    margin: '2px 0px 2px 0px'
+  }
+
+  return (
+    <div style={style}>
+      {blog.title} {blog.author}
+      <button style={hideWhenVisible} onClick={togglevisibility}>show</button>
+      <button style={showWhenVisible} onClick={togglevisibility}>hide</button>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes}<button>like</button></div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
