c00e675a06244c194d8cbd9b03e6c4b7fd0e28d9
Leemuns <firedew123@gmail.com>
2026-06-07 23:46:30 +0800

    Add style to single blog display (exercise 5.31)

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 
 import Togglable from './Togglable'
+import { Card, CardContent, Typography, Button, Link } from '@mui/material'
 
 const Blog = ({ blog, likeBlog, removeBlog, userId }) => {
   const navigate = useNavigate()
@@ -15,18 +16,29 @@ const Blog = ({ blog, likeBlog, removeBlog, userId }) => {
   }
 
   return (
-    <div className='blog'>
-      <h2>{blog.title} {blog.author}</h2>
-      <div>
-        <div>{blog.url}</div>
-        <div>
-          <span>likes {blog.likes}</span>
-          <button onClick={handleLike} style={showIfLoggedIn}>like</button>
-        </div>
-        <div>{blog.user.name}</div>
-        <button onClick={handleRemove} style={showIfUserMatch}>remove</button>
-      </div>
-    </div>
+    <Card className='blog'>
+      <CardContent>
+        <Typography variant='h5' sx={{ mb: 1.5 }}>
+          {blog.title}
+        </Typography>
+        <Typography variant='body1' sx={{ color: 'text.secondary', mb: 1 }}>
+          by {blog.author}
+        </Typography>
+        <Typography variant='body2' sx={{ color: 'info.main', textDecoration: 'underline', mb: 1 }}>
+          <Link href={blog.url} target="_blank" >
+            {blog.url}
+          </Link>
+        </Typography>
+        <Typography variant='body2' sx={{ color: 'text.secondary', mb: 1 }}>
+          Added by {blog.user.name}
+        </Typography>
+        <Typography variant='body1'>
+          {blog.likes} likes
+          <Button onClick={handleLike} variant='outlined' size='small' sx={{ ml: 1, }} style={showIfLoggedIn}>like</Button>
+          <Button onClick={handleRemove} variant='outlined' size='small' sx={{ ml: 1 }} color='error' style={showIfUserMatch}>remove</Button>
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
