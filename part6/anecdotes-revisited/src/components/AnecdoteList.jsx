079c52b777d2126100c95bedf922870c8b1055bc
Leemuns <firedew123@gmail.com>
2026-06-12 16:05:41 +0800

    Modify anecdoteList to order anecdotes by votes in descending order (exercise 6.5)

--- part6/anecdotes-revisited/src/components/AnecdoteList.jsx
+++ part6/anecdotes-revisited/src/components/AnecdoteList.jsx
@@ -10,15 +10,16 @@ const AnecdoteList = () => {
 
   return (
     <div>
-      {anecdotes.map(anecdote => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
+      {anecdotes.toSorted((a, b) => b.votes - a.votes)
+          .map(anecdote => (
+            <div key={anecdote.id}>
+              <div>{anecdote.content}</div>
+              <div>
+                has {anecdote.votes}
+                <button onClick={() => vote(anecdote.id)}>vote</button>
+              </div>
+            </div>
+          ))}
     </div>
   )
 }
