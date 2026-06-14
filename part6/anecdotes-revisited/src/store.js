0b60585d445057c76e9420df9bbf6ec8ab9f5654
Leemuns <firedew123@gmail.com>
2026-06-15 00:13:13 +0800

    Fix bug where notification clears earlier than expected (exercise 6.11)

--- part6/anecdotes-revisited/src/store.js
+++ part6/anecdotes-revisited/src/store.js
@@ -16,21 +16,18 @@ const useAnecdoteStore = create((set, get) => ({
       const anecdote = get().anecdotes.find(a => a.id === id)
       const updated = {...anecdote, votes: anecdote.votes + 1}
       const saved = await anecdoteService.update(id, updated)
-      const { display } = useNotificationStore.getState().actions
       set(state => ({ anecdotes: state.anecdotes.map(a => a.id === id ? saved : a) }))
-      display(`You voted "${truncate(anecdote.content)}".`)
+      useNotificationStore.getState().actions.display(`You voted "${truncate(anecdote.content)}".`)
     },
     add: async content => {
       const newAnecdote = await anecdoteService.create({ content, id: getId(), votes: 0 })
-      const { display } = useNotificationStore.getState().actions
       set(state => ({ anecdotes: state.anecdotes.concat(newAnecdote)}))
-      display(`You added "${truncate(newAnecdote.content)}".`)
+      useNotificationStore.getState().actions.display(`You added "${truncate(newAnecdote.content)}".`)
     },
     remove: async id => {
       const removedAnecdote = await anecdoteService.remove(id)
       set(state => ({ anecdotes: state.anecdotes.filter(a => a.id !== id) }))
-      const { display } = useNotificationStore.getState().actions
-      display(`You removed "${truncate(removedAnecdote.content)}".`)
+      useNotificationStore.getState().actions.display(`You removed "${truncate(removedAnecdote.content)}".`)
     },
     setFilter: value => set(() => ({ filter: value})),
     initialize: async () => {
@@ -40,16 +37,19 @@ const useAnecdoteStore = create((set, get) => ({
   }
 }))
 
-// I have hated this notification thing since part 2
-const useNotificationStore = create((set) => ({
-  notification: null,
-  actions: {
-    display: message => {
-      set(() => ({ notification: message}))
-      setTimeout(() => set(() => ({ notification: null })), 5000)
+const useNotificationStore = create((set) => {
+  var notifTimeout
+  return {
+    notification: null,
+    actions: {
+      display: message => {
+        set(() => ({ notification: message}))
+        clearTimeout(notifTimeout)
+        notifTimeout = setTimeout(() => set(() => ({ notification: null })), 5000)
+      }
     }
   }
-}))
+})
 
 export const useAnecdotes = () => {
   const anecdotes = useAnecdoteStore((state) => state.anecdotes)
