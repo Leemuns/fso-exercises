f70b6e25af742cad45ae44318a7134ec624fcd9f
Leemuns <firedew123@gmail.com>
2026-06-15 14:58:27 +0800

    Add tests to check if filters works on store anecdotes (exercise 6.14)

--- part6/anecdotes-revisited/src/store.test.js
+++ part6/anecdotes-revisited/src/store.test.js
@@ -15,7 +15,7 @@ beforeEach(() => {
   vi.clearAllMocks()
 })
 
-describe('anecdote store', () => {
+describe('anecdote store hooks', () => {
   it('initialize anecdotes from backend', async () => {
     const mockAnecdotes = [{ id: 1, content: 'Test anecdote.', votes: 4 }]
     anecdoteService.getAll.mockResolvedValue(mockAnecdotes)
@@ -32,7 +32,7 @@ describe('anecdote store', () => {
   it('Components receive anecdotes sorted by votes', async () => {
     const mockAnecdotes = [
       { id: 1, content: 'Test anecdote.', votes: 4 },
-      { id: 2, content: 'Bording anecdote.', votes: 2 },
+      { id: 2, content: 'Boring anecdote.', votes: 2 },
       { id: 3, content: 'Interesting anecdote.', votes: 7 }
     ]
     anecdoteService.getAll.mockResolvedValue(mockAnecdotes)
@@ -45,5 +45,31 @@ describe('anecdote store', () => {
 
     const { result: anecdotesResult } = renderHook(() => useAnecdotes())
     expect(anecdotesResult.current).toStrictEqual(mockAnecdotes.toSorted((a, b) => b.votes - a.votes))
+  })
+
+  describe('useAnecdote filtering', () => {
+    const anecdotes = [
+      { id: 1, content: 'Test anecdote.', votes: 4 },
+      { id: 2, content: 'Test Boring anecdote.', votes: 2 },
+      { id: 3, content: 'Interesting anecdote.', votes: 7 }
+    ]
+
+    beforeEach(() => {
+      useAnecdoteStore.setState({ anecdotes })
+    })
+
+    it('returns all anecdotes with no filter', () => {
+      const { result } = renderHook(() => useAnecdotes())
+      expect(result.current).toHaveLength(3)
+    })
+
+    it('filters test anecdotes', () => {
+      useAnecdoteStore.setState({ filter: 'test' })
+      const { result } = renderHook(() => useAnecdotes())
+      expect(result.current).toHaveLength(2)
+      expect(result.current).toContainEqual(anecdotes[0])
+      expect(result.current).toContainEqual(anecdotes[1])
+      expect(result.current).not.toContainEqual([anecdotes[2]])
+    })
   })
 })
